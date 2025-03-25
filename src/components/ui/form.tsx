d227d25a957527c11a6af11bd9f030e6def231f6
Augustusdngqt <tnduykhanhdng@gmail.com>
2025-03-25 14:36:40 +0700

    fix: bug view message error of formzod when error is array

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -115,7 +115,7 @@ FormDescription.displayName = 'FormDescription'
 const FormMessage = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
   ({ className, children, ...props }, ref) => {
     const { error, formMessageId } = useFormField()
-    const body = error ? String(error?.message) : children
+    const body = error ? (Array.isArray(error) ? error[0]?.message : error?.message) : children
 
     if (!body) {
       return null
